5ca40d968872f8eaec79b56951121e850089db4f
Jorge Subeldia <54650566+jhorgesub@users.noreply.github.com>
2026-02-02 11:02:24 -0300

    Modificado app.routes.server.ts

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,6 +2,11 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'project-details/:id',
+    renderMode: RenderMode.Server
+  },
+
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
